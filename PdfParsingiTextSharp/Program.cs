4db7fa02339d5ff1a4eea32cec3e1683fc94219e
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2020-01-07 15:56:23 +0100

    Cleaning and a list
    Ta daaah!

--- PdfParsingiTextSharp/Program.cs
+++ PdfParsingiTextSharp/Program.cs
@@ -13,58 +13,28 @@ namespace PdfParsingiTextSharp
         static void Main(string[] args)
         {
             //String nameComponent = "MINE";
-            // Rename the files
+            //Rename the files
             //System.IO.File.Move("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/vmi.pdf", "C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/asd"+ nameComponent + ".pdf");
 
-
-            string fullText = ExtractTextFromPdf("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/20200107_example.pdf");
+            //Use that functions
+            string fullText = Transform("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/20200107_example.pdf");
             string mergedIDs = IDNumberizator(fullText);
+            List<string> listedIDs = mergedIDs.Split('\n').ToList();
 
-            /*
-
-            string startID = "adóazonosító jele: ";
-            string endID = " TAJ";
-            //int textLength = fullText.Length;
-            int startIndex, endIndex;
-            bool status = true;
-
-            while(status == true)
-            { 
-                if (fullText.Contains(startID) && fullText.Contains(endID))
-                {
-                    startIndex = fullText.IndexOf(startID, 0) + startID.Length;
-                    endIndex = fullText.IndexOf(endID, startIndex);
-                    //Console.WriteLine(startIndex); // Kiirja a megtalált adószám text-béli kezdő indexét
-                    //Console.WriteLine(endIndex); // Kiirja a megtalált adószám text-béli végződő indexét
-                    mergedIDs += fullText.Substring(startIndex, endIndex - startIndex) + "\n";
-                    //Console.WriteLine(fullText.Substring(startIndex, endIndex - startIndex)); //Ezt az adatot kell listában eltárolni!
-                    //Console.WriteLine(fullText.Length);
-                    fullText = fullText.Remove(0, endIndex);  //(endIndex);
-                    //Console.WriteLine(fullText.Length);
-                }
-                else
-                {
-                    //Console.WriteLine("IDnumber is missing.");
-                    status = false;
-                }
+            for (int i=0; i<listedIDs.Count; i++)
+            {
+                Console.WriteLine(listedIDs[i]);
             }
-            */
 
-            Console.WriteLine(mergedIDs);
 
-            //Destroyolt függvény maradékai.
-            //String IDnumber = getBetween(text, start, end);
-            //System.IO.File.WriteAllText("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/Fileslist.txt", IDnumber);
-            //IDnumbers.Add(text);  // Szerintem ehhez nem is kell lista
-
+            //Console.WriteLine(mergedIDs);
             System.IO.File.WriteAllText("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/done.txt", mergedIDs);
-            //Console.WriteLine(text);
             Console.ReadLine();
         }
 
         //ezek nem a mainben fognak szerepelni, hanem egy külön fileban
 
-        public static string ExtractTextFromPdf(string path)
+        public static string Transform(string path)
         {
             using (PdfReader reader = new PdfReader(path))
             {
@@ -74,7 +44,6 @@ namespace PdfParsingiTextSharp
                 {
                     text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
                 }
-
                 return text.ToString();
             }
         }
@@ -103,25 +72,5 @@ namespace PdfParsingiTextSharp
             }
             return mergeIDs;
         }
-
-
-        /*
-        public static string getBetween(string strSource, string strStart, string strEnd)
-        {
-            int Start, End;
-            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
-            {
-                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
-                End = strSource.IndexOf(strEnd, Start);
-                Console.WriteLine(Start); // Kiirja a megtalált adószám text-béli kezdő indexét
-                Console.WriteLine(End); // Kiirja a megtalált adószám text-béli végződő indexét
-                Console.WriteLine(strSource.Substring(Start, End - Start));
-                return strSource.Substring(Start, End - Start);
-            }
-            else
-            {
-                return "";
-            }
-        }*/
     }
 }
