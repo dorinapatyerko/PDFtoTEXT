d933656ae0198cfc3bafae49ccb64157f753298e
Dorina Pátyerkó <dorinapatyerko@gmail.com>
2020-01-08 14:15:45 +0100

    First working version
    I must use a PDF split program for that. Recommended: PDFsam.
    Split the whole *.pdf to a lot *.pdfs (1pdf/1page). Then rename that to the recommanded format. (for example: 8198040935_00000000_201912)

--- PdfParsingiTextSharp/Program.cs
+++ PdfParsingiTextSharp/Program.cs
@@ -12,27 +12,45 @@ namespace PdfParsingiTextSharp
     {
         static void Main(string[] args)
         {
-            //String nameComponent = "MINE";
-            //Rename the files
-            //System.IO.File.Move("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/vmi.pdf", "C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/asd"+ nameComponent + ".pdf");
-
-            //Use that functions
+            // Use that functions
             string fullText = Transform("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/20200107_example.pdf");
-            string mergedIDs = IDNumberizator(fullText);
-            List<string> listedIDs = mergedIDs.Split('\n').ToList();
+            string mergedIDs = IDNumberizator(fullText, "adóazonosító jele: ", " TAJ");
+            string mergedDates = IDNumberizator(fullText, "Dátum: ", " Oldal");
+            string rightDate = mergedDates[0].ToString() + mergedDates[1].ToString() + mergedDates[2].ToString() + mergedDates[3].ToString() + mergedDates[5].ToString() + mergedDates[6].ToString();
 
+            // Transform the string to a list
+            List<string> listedIDs = mergedIDs.Split('\n').ToList();
             for (int i=0; i<listedIDs.Count; i++)
             {
                 Console.WriteLine(listedIDs[i]);
             }
 
+            // Final *.pdf name format example: 8198040935_00000000_201912 (~ aka: ID_[0..0]_YYYYMM)
+            //TODO string Date from the *.pdf -> format: YYYYMM
+            //TODO use the second component (00000000) as serial number. For example: 00000001 -> 
+            //     if I have more than 9 file with tha same IDnumber it will run with an error, 
+            //     so I must handle that with an Exception.
+            int j = 0;
+            for (int i = 1; i < listedIDs.Count; i++) 
+            {
+                System.IO.File.Move("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/Splitted/" + i + "_justafool_20200107_example.pdf", "C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/Splitted/" + listedIDs[i-1] + "_" + "0000000" + j + "_" + rightDate + ".pdf");
+                if (i + 1 != listedIDs.Count) 
+                { 
+                    if (listedIDs[i-1] == listedIDs[i]) j++;
+                    if (listedIDs[i-1] != listedIDs[i]) j = 0;
+                }
+            }
 
-            //Console.WriteLine(mergedIDs);
+            //String nameComponent = "MINE";
+            //Rename the files
+            //System.IO.File.Move("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/vmi.pdf", "C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/asd"+ nameComponent + ".pdf");
+
+            Console.WriteLine(rightDate);
             System.IO.File.WriteAllText("C:/Users/patydor/Google Drive/WORK/Notes/Progger Projects/PDFtoTEXT/Files/done.txt", mergedIDs);
             Console.ReadLine();
         }
 
-        //ezek nem a mainben fognak szerepelni, hanem egy külön fileban
+        //TODO replace these functions an another *.cs file
 
         public static string Transform(string path)
         {
@@ -48,11 +66,11 @@ namespace PdfParsingiTextSharp
             }
         }
 
-        public static string IDNumberizator(string text)
+        public static string IDNumberizator(string text, string startID, string endID)
         {
             string mergeIDs="";
-            string startID = "adóazonosító jele: ";
-            string endID = " TAJ";
+            //string startID = "adóazonosító jele: ";
+            //string endID = " TAJ";
             int startIndex, endIndex;
             bool status = true;
 
